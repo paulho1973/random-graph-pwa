504d37dafadd81c26d20bae1637feb3be2a219d5
YONG HING HO <paulho1973@outlook.com>
2022-02-15 14:16:20 +0800

    commit 6

--- src/Qrscan/index.js
+++ src/Qrscan/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader';
 
+import styles from './Qrscan.module.css';
 
 const Qrscan = () => {
+
+	const [result, setResult] = useState('No result');
+
+	const handleError = (err) => {
+		console.err(err)
+	}
+
+	const handleScan = (result) => {
+		if(result){
+			setResult(result)
+		}
+	}
+
+	const previewStyle = {
+		height: 240,
+		width: 320,
+	}
+
 	return (
-		<div>
-			<h1>QR SCAN</h1>		
+		<div className={styles.container}>
+			<QrReader
+			delay={500}
+			style={previewStyle}
+			onError={handleError}
+			onScan={handleScan}
+			/>
+			<div className={styles.result}>{result}</div>		
 		</div>
 	);
 }
